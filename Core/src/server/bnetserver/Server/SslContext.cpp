da6f4df72215c419426b63bac81faa966212dcb7
fluxurion <zoltan@fatalerror.hu>
2025-03-29 19:36:33 +0100

    Fix logging in SslContext.cpp

--- Core/src/server/bnetserver/Server/SslContext.cpp
+++ Core/src/server/bnetserver/Server/SslContext.cpp
@@ -64,7 +64,7 @@ bool Battlenet::SslContext::Initialize()
 #define LOAD_CHECK(fn) do { fn; \
     if (err) \
     { \
-        TC_LOG_ERROR("server.ssl", #fn " failed: {}", err.message()); \
+        TC_LOG_ERROR("server.ssl", #fn " failed: %s", err.message()); \
         return false; \
     } } while (0)
 
